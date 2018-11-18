fd9d168f1a94c0d1571a3a6560b352d2630c4e01
AZelmar <arnaud.zelmar@isep.fr>
2018-11-18 22:09:28 +0100

    Création basique de la page php

---
+++ forgotPassword.php
@@ -0,0 +1,14 @@
+<?php
+include 'config.php';
+?>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="style.css" /> 
+	<title>SweetHouse | Mot de passe oublié</title>
+</head>
+<body>
+	<?php include 'header.php' ?>
+	<?PHP include 'notification.php' ?>
+</body>
+</html>
