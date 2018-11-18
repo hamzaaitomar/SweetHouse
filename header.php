5d498a9cfff293931cf57ba7cecf0b42ba4ff9e9
AZelmar <arnaud.zelmar@isep.fr>
2018-11-18 22:20:10 +0100

    Passage des noms de fichiers en anglais

--- header.php
+++ header.php
@@ -5,8 +5,8 @@
   <ul>
   <li id="logo-li"><img id="logo" src="logo.png"></li>
   <li><a href="#accueil"><p>Accueil</p></a></li>
-  <li><a href="inscription.php"><p>Inscription</p></a></li>
-  <li><a href="connexion.php"><p>Connexion</p></a></li>
+  <li><a href="register.php"><p>Inscription</p></a></li>
+  <li><a href="login.php"><p>Connexion</p></a></li>
   <li><a href="#aide"><p>Aide</p></a></li>
   <li><a href="#contact"><p>Contactez-nous !</p></a></li>
 	</ul>
@@ -20,7 +20,7 @@ else
   <li><a href="#accueil"><p>Accueil</p></a></li>
   <li><a href="#inscription"><p>Gestion Profil</p></a></li>
   <li><a href="#connexion"><p>Gestion Maison</p></a></li>
-  <li><a href="deconnexion.php"><p>Déconnexion</p></a></li>
+  <li><a href="logout.php"><p>Déconnexion</p></a></li>
   <li><a href="#contact"><p>Contactez-nous !</p></a></li>
   </ul>
   <?php
